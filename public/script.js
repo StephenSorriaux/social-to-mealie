ebb9a8e00afcdfa0f4af397ef60899793e8f3529
gerardPolloRebozado <gerardit4@gmail.com>
2025-02-06 16:36:51 +0100

    feat: :sparkles: Added spinner animation when sending the link

--- public/script.js
+++ public/script.js
@@ -1,8 +1,12 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const body = document.querySelector('body');
+const spinner = document.getElementById('spinner');
+
+body.removeChild(spinner);
 
 button.addEventListener('click', async () => {
+  body.appendChild(spinner);
   const res = await fetch('/get-url', {
     method: 'POST',
     headers: {
@@ -13,10 +17,12 @@ button.addEventListener('click', async () => {
   const data = await res.json();
   if (data.error) {
     alert(data.error);
+    body.removeChild(spinner);
     return;
   }
   const newLink = document.createElement('a');
   newLink.href = data.data;
   newLink.innerHTML = 'Mealie created recipe';
   body.appendChild(newLink);
+  body.removeChild(spinner);
 });
